4975a96a153bd3d330e98d34e8076617b550e474
Chris Cooper <Christopher.Cooper@vu.com>
2025-04-01 18:11:52 -0500

    This one was inefficient. I will clean up the next ones

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,6 +2,17 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+    n = int(in_num)
+    dif = n - 21
+    clean = abs(dif)
+    twodif = 2 * dif
+
+    if n > 21:
+        print(twodif)
+    else: 
+        print(clean)
+
+
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
